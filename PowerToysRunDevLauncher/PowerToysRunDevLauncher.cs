cef0ec3eb356c95267112a0f4b77553036a97973
Cody Owens <owenscody@gmail.com>
2021-01-15 21:29:59 -0600

    Get debug working

--- PowerToysRunDevLauncher/PowerToysRunDevLauncher.cs
+++ PowerToysRunDevLauncher/PowerToysRunDevLauncher.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using Wox.Plugin;
 
 namespace PowerToysRunDevLauncher
 {
-	public class PowerToysRunDevLauncher
+	public class PowerToysRunDevLauncher : IPlugin
 	{
+		public void Init( PluginInitContext context )
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Result> Query( Query query )
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
